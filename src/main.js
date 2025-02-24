6397074a26ce5e833365c1cc9c9a359ddd99acd8
A. Davis <adavis@esri.com>
2025-02-24 10:42:12 -0800

    Add demo app files; configure api key

--- src/main.js
+++ src/main.js
@@ -1,24 +1,176 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/**
+ * This example uses the Distribution build of Calcite Components.
+ * Refer to the documentation if switching to the Custom Elements build:
+ * https://developers.arcgis.com/calcite-design-system/get-started/#choose-a-build
+ **/
+import "@esri/calcite-components/dist/calcite/calcite.css";
+import { defineCustomElements } from "@esri/calcite-components/dist/loader";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+/**
+ * ES Modules from the JS Maps SDK
+ */
+import esriConfig from "@arcgis/core/config";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import { addressToLocations } from "@arcgis/core/rest/locator";
+import Graphic from "@arcgis/core/Graphic";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import * as geometryEngine from "@arcgis/core/geometry/geometryEngine";
+import * as farmBuildingCIMSymbol from "./farm-building-cim-symbol.json";
+import * as homeCIMSymbol from "./home-cim-symbol.json";
+
+  
+// Load calcite components
+defineCustomElements(window, {
+  resourcesUrl: "https://js.arcgis.com/calcite-components/2.6.0/assets",
+});
+
+// Add api key to access basemaps service
+esriConfig.apiKey = import.meta.env.VITE_ARCGIS_API_KEY;
+
+let pointGraphic;
+let csaPickupsLayerView;
+const map = new Map({
+  basemap: "arcgis/community",
+});
+
+const view = new MapView({
+  container: "viewDiv",
+  map,
+  center: [-122.676483, 45.523],
+  zoom: 12,
+  highlightOptions: {
+    color: "#B20D30",
+    haloOpacity: 0.75,
+  },
+  popup: {
+    dockOptions: {
+      buttonEnabled: false
+    },
+    visibleElements: {
+      actionBar: false,
+      collapseButton: false,
+      featureNavigation: false
+    },
+  },
+});
+
+const csaRenderer = {
+  type: "simple",
+  symbol: {
+    type: "cim",
+    data: {
+      type: "CIMSymbolReference",
+      symbol: farmBuildingCIMSymbol
+    },
+  },
+};
+
+const csaPopup = {
+  title: "{Farm_Name}",
+  content:
+    "<b>Pickup address: </b>{Location}<br/><br/><a href={Website}>View website</a>",
+  
+};
+
+const graphicsLayer = new GraphicsLayer();
+
+const csaPickupsLayer = new FeatureLayer({
+  url: "https://www.portlandmaps.com/od/rest/services/COP_OpenData_ImportantPlaces/MapServer/188",
+  renderer: csaRenderer,
+  popupTemplate: csaPopup
+});
+
+map.add(graphicsLayer);
+map.add(csaPickupsLayer);
+
+try {
+  csaPickupsLayerView = await view.whenLayerView(csaPickupsLayer);
+} catch (error) {
+  console.log(error);
+}
+
+
+// calcite-input custom event; fires on submit/enter key press
+document.addEventListener("calciteInputChange", async (event) => {
+  const address = { singleLine: event.target.value };
+  const serviceUrl =
+    "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+
+  try {
+    // use the Maps SDK's locator helper to call the REST API's geocoding service
+    const locations = await addressToLocations(serviceUrl, { address });
+    if (locations.length === 0) {
+      return;
+    }
+
+    // Get the location from the top match
+    const addressLocation = locations[0].toJSON().location;
+
+    // Convert location's geometry to a point graphic and add to map
+    addHomeGraphic(addressLocation)
+
+    const buffer = createBuffer(pointGraphic);
+    filterByLocation(buffer);
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+// calcite-slider custom event; fires only when slider's handle is released
+document.addEventListener("calciteSliderChange", () => {
+  if (!pointGraphic) {
+    return;
+  }
+  const buffer = createBuffer(pointGraphic);
+  filterByLocation(buffer);
+});
+
+function addHomeGraphic (location) {
+  // Clear any previous point from the map
+  graphicsLayer.removeAll();
+
+  const { x, y } = location;
+
+  pointGraphic = new Graphic({
+    geometry: { type: "point", x, y },
+    symbol: {
+      type: "cim",
+      data: {
+        type: "CIMSymbolReference",
+        symbol: homeCIMSymbol,
+      },
+    }
+  });
+  graphicsLayer.add(pointGraphic);
+  view.goTo(pointGraphic);
+}
+
+
+function createBuffer (point) {
+  const distance = document.getElementById("distance-slider").value;
+
+  return geometryEngine.geodesicBuffer(
+    point.geometry,
+    distance,
+    "miles"
+  )
+}
+
+function filterByLocation (geometry) {
+  if (!csaPickupsLayer) {
+    return;
+  }
+  const featureFilter = {
+    geometry,
+    spatialRelationship: "intersects",
+    units: "miles"
+  };
+
+  csaPickupsLayerView.featureEffect = {
+    filter: featureFilter,
+    excludedEffect: "grayscale(100%) opacity(40%)"
+  }
+}
