6b4adb4f2bb97072cffd8abc52da304aa8f5c262
A. Davis <adavis@esri.com>
2025-03-06 16:42:51 -0800

    Fix popup performance and styling; reorganize and cleanup app code; update resource list

--- src/main.js
+++ src/main.js
@@ -6,171 +6,211 @@
 import "@esri/calcite-components/dist/calcite/calcite.css";
 import { defineCustomElements } from "@esri/calcite-components/dist/loader";
 
-
 /**
  * ES Modules from the JS Maps SDK
  */
 import esriConfig from "@arcgis/core/config";
-import Map from "@arcgis/core/Map";
-import MapView from "@arcgis/core/views/MapView";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
-import { addressToLocations } from "@arcgis/core/rest/locator";
-import Graphic from "@arcgis/core/Graphic";
-import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
-import * as geometryEngine from "@arcgis/core/geometry/geometryEngine";
-import * as farmBuildingCIMSymbol from "./farm-building-cim-symbol.json";
-import * as homeCIMSymbol from "./home-cim-symbol.json";
+import * as promiseUtils from "@arcgis/core/core/promiseUtils.js";
 
-  
+/**
+ * Map components
+ */
+import "@arcgis/map-components/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-zoom";
+import "@arcgis/map-components/components/arcgis-feature";
+
+/**
+ * Custom symbols
+ */
+import * as farmBuildingCIMSymbol from "./farm-building-cim-symbol.json";
+
+/**
+ * Shared functions from list-view
+ */
+import { normalizeFeatureData, displayCard, categorizeProducts, getChips } from "./list-view/list-view";
+
 // Load calcite components
 defineCustomElements(window, {
-  resourcesUrl: "https://js.arcgis.com/calcite-components/2.6.0/assets",
+  resourcesUrl: "https://js.arcgis.com/calcite-components/3.0.3/assets",
 });
 
-// Add api key to access basemaps service
-esriConfig.apiKey = import.meta.env.VITE_ARCGIS_API_KEY;
-
-let pointGraphic;
-let csaPickupsLayerView;
-const map = new Map({
-  basemap: "arcgis/community",
-});
-
-const view = new MapView({
-  container: "viewDiv",
-  map,
-  center: [-122.676483, 45.523],
-  zoom: 12,
-  highlightOptions: {
-    color: "#B20D30",
-    haloOpacity: 0.75,
-  },
-  popup: {
-    dockOptions: {
-      buttonEnabled: false
-    },
-    visibleElements: {
-      actionBar: false,
-      collapseButton: false,
-      featureNavigation: false
-    },
-  },
-});
-
+// Set up renderer with custom CIMSymbol
 const csaRenderer = {
   type: "simple",
   symbol: {
     type: "cim",
     data: {
       type: "CIMSymbolReference",
-      symbol: farmBuildingCIMSymbol
+      symbol: farmBuildingCIMSymbol,
     },
   },
 };
 
+function customizePopupContent(feature) {
+  const products = categorizeProducts(feature.graphic.attributes["Main_Products"]);
+  const chips = getChips(products);
+  return `<p><b>Pickup address:</b> {Location}</p><ul class="popup-chips">${chips}</ul><a href={Website}>View website</a>`;
+}
+// Configure popup template content
 const csaPopup = {
   title: "{Farm_Name}",
-  content:
-    "<b>Pickup address: </b>{Location}<br/><br/><a href={Website}>View website</a>",
-  
+  content: customizePopupContent
 };
 
-const graphicsLayer = new GraphicsLayer();
-
+// Configure the CSA pickups feature layer
 const csaPickupsLayer = new FeatureLayer({
   url: "https://www.portlandmaps.com/od/rest/services/COP_OpenData_ImportantPlaces/MapServer/188",
   renderer: csaRenderer,
-  popupTemplate: csaPopup
+  popupTemplate: csaPopup,
 });
 
-map.add(graphicsLayer);
-map.add(csaPickupsLayer);
+const mapElement = document.querySelector("arcgis-map");
+const feature = document.querySelector("arcgis-feature");
+const container = document.querySelector(".card-container");
 
-try {
-  csaPickupsLayerView = await view.whenLayerView(csaPickupsLayer);
-} catch (error) {
-  console.log(error);
-}
+const defaultGraphic = {
+  popupTemplate: {
+    content: "Hover over a pickup site to show details.",
+  },
+};
 
+// Wait until map component is ready before we begin working with it
+mapElement.addEventListener("arcgisViewReadyChange", async (event) => {
+  // Set our API key in esri config to access basemaps service
+  esriConfig.apiKey = import.meta.env.VITE_ARCGIS_API_KEY;
+  mapElement.basemap = "arcgis/community";
 
-// calcite-input custom event; fires on submit/enter key press
-document.addEventListener("calciteInputChange", async (event) => {
-  const address = { singleLine: event.target.value };
-  const serviceUrl =
-    "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+  mapElement.highlights = [
+    {
+      name: "default",
+      color: "#B20D30",
+      haloOpacity: 0.75,
+    },
+  ];
 
-  try {
-    // use the Maps SDK's locator helper to call the REST API's geocoding service
-    const locations = await addressToLocations(serviceUrl, { address });
-    if (locations.length === 0) {
-      return;
-    }
+  // Add our layer to the map with all data fields
+  mapElement.addLayer(csaPickupsLayer);
+  csaPickupsLayer.outFields = ["*"];
 
-    // Get the location from the top match
-    const addressLocation = locations[0].toJSON().location;
+  // Query, process, and display our layer data with a promise chain - will it work?
+  // const features = csaPickupsLayer.queryFeatures({
+  //   where: "Status = 'Active'",
+  //   returnGeometry: false,
+  //     outFields: [
+  //       "OBJECTID",
+  //       "Farm_Name",
+  //       "FarmDescript",
+  //       "Location",
+  //       "Main_Products",
+  //       "Website",
+  //       "email",
+  //     ]
+  // })
+  // .then((data) => {
+  //   return normalizeFeatureData(data);
+  // })
+  // .then((data) => {
+  //   console.log("Data inside promise chain", data);
+  //   container.replaceChildren("");
+  //   data.forEach((feature) => {
+  //     displayCard(feature);
+  //   });
+  //   return data;
+  // });
 
-    // Convert location's geometry to a point graphic and add to map
-    addHomeGraphic(addressLocation)
+  // Query, process, and display our layer data with async/await
+  const featureSet = await csaPickupsLayer.queryFeatures({
+    where: "Status = 'Active'",
+    returnGeometry: false,
+    outFields: [
+      "OBJECTID",
+      "Farm_Name",
+      "FarmDescript",
+      "Location",
+      "Main_Products",
+      "Website",
+      "email",
+    ]
+  });
 
-    const buffer = createBuffer(pointGraphic);
-    filterByLocation(buffer);
-  } catch (error) {
-    console.log(error)
-  }
-});
+  const features = normalizeFeatureData(featureSet);
 
-// calcite-slider custom event; fires only when slider's handle is released
-document.addEventListener("calciteSliderChange", () => {
-  if (!pointGraphic) {
-    return;
-  }
-  const buffer = createBuffer(pointGraphic);
-  filterByLocation(buffer);
-});
+  container.replaceChildren("");
+  features.forEach((feature) => {
+    displayCard(feature);
+  });
 
-function addHomeGraphic (location) {
-  // Clear any previous point from the map
-  graphicsLayer.removeAll();
+  // show the default graphic after the map loads (before we set up the hit test)
+  feature.graphic = defaultGraphic;
+  
+  // Wait for the layer view to be ready before setting up the hitTest below
+  const csaPickupsLayerView = await mapElement.whenLayerView(csaPickupsLayer);
 
-  const { x, y } = location;
+  let highlight, objectId;
 
-  pointGraphic = new Graphic({
-    geometry: { type: "point", x, y },
-    symbol: {
-      type: "cim",
-      data: {
-        type: "CIMSymbolReference",
-        symbol: homeCIMSymbol,
-      },
+  // Wrap hit test in JS SDK's debounce util to ensure input function isn't invoked more than once at a time: https://developers.arcgis.com/javascript/latest/api-reference/esri-core-promiseUtils.html#debounce
+  const debouncedGraphicUpdate = promiseUtils.debounce(async (event) => {
+    const hitTest = await mapElement.hitTest(event, {
+      include: csaPickupsLayer,
+    });
+
+    const results = hitTest.results.filter((result) => {
+      return result.graphic.layer.popupTemplate;
+    });
+
+    const result = results[0];
+    const newId = result?.graphic.attributes[csaPickupsLayer.objectIdField];
+
+    if (!newId) {
+      highlight?.remove();
+      objectId = feature.graphic = defaultGraphic;
+    } else if (objectId !== newId) {
+      highlight?.remove();
+      objectId = newId;
+      feature.graphic = result.graphic;
+      highlight = csaPickupsLayerView.highlight(result.graphic);
     }
   });
-  graphicsLayer.add(pointGraphic);
-  view.goTo(pointGraphic);
-}
 
+  mapElement.addEventListener("arcgisViewPointerMove", (event) => {
+    debouncedGraphicUpdate(event.detail).catch((error) => {
+      // Check if the error is caused by a rejected/aborted promise. If it is not, throw the error
+      if (!promiseUtils.isAbortError(error)) {
+        throw error;
+      }
+    });
+  });
 
-function createBuffer (point) {
-  const distance = document.getElementById("distance-slider").value;
+  document.addEventListener("calciteChipGroupSelect", (event) => {
+    container.replaceChildren("");
 
-  return geometryEngine.geodesicBuffer(
-    point.geometry,
-    distance,
-    "miles"
-  )
-}
+    // Set filter values from the selected chips
+    const productFilter = event.target.selectedItems.map(
+      (selected) => selected.value
+    );
 
-function filterByLocation (geometry) {
-  if (!csaPickupsLayer) {
-    return;
-  }
-  const featureFilter = {
-    geometry,
-    spatialRelationship: "intersects",
-    units: "miles"
-  };
+    const featureFilter = {
+      where: "Main_Products LIKE '%" + productFilter.join(", ") + "%'",
+    };
 
-  csaPickupsLayerView.featureEffect = {
-    filter: featureFilter,
-    excludedEffect: "grayscale(100%) opacity(40%)"
-  }
-}
+    csaPickupsLayerView.featureEffect = {
+      filter: featureFilter,
+      includedEffect: "bloom(10%)",
+      excludedEffect: "sepia(100%) opacity(30%)",
+    };
+
+    // Logging out feature data before we filter out cards with no matching products
+    console.log("Data inside our event handler", features);
+
+    features.forEach((feature) => {
+      const isMatch =
+        feature.products.filter((product) =>
+          productFilter.includes(product.replace(" ", "_"))
+        ).length === productFilter.length;
+      if (isMatch) {
+        displayCard(feature);
+      }
+    });
+  });
+});
